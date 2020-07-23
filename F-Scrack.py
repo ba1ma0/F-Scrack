780888defc4797a8936db573f1443edbca2f8cb9
ba1ma0 <1058763824@qq.com>
2020-07-23 22:12:59 +0800

    优化了一下工具

--- F-Scrack.py
+++ F-Scrack.py
@@ -380,7 +380,8 @@ def scan_port(host,port):
         return 'NULL'
 def log(scan_type,host,port,info=''):
     mutex.acquire()
-    time_str = time.strftime('%X', time.localtime( time.time()))
+    #time_str = time.strftime('%X', time.localtime( time.time()))
+    time_str = time.strftime("%Y-%m-%d %X",time.localtime())
     if scan_type == 'portscan':
         print "[%s] %s:%d open"%(time_str,host,int(port))
     elif scan_type == 'discern':
@@ -481,7 +482,20 @@ def put_queue(ip_list,port_list):
             queue.put(":".join(['portscan',ip,port]))
 if __name__=="__main__":
     msg = '''
-Usage: python F-Scrack.py -h 192.168.1 [-p 21,80,3306] [-m 50] [-t 10] [-d pass.txt] [-n]
+Usage:
+        python F-Scrack.py -h 192.168.1 [-p 21,80,3306] [-m 50] [-t 10]
+        -h 必须输入的参数，支持ip(192.168.1.1)，ip段（192.168.1），ip范围指定（192.168.1.1-192.168.1.254）,ip列表文件（ip.ini），最多限制一次可扫描65535个IP。
+        -p 指定要扫描端口列表，多个端口使用,隔开 例如：1433,3306,5432。未指定即使用内置默认端口进行扫描(21,23,1433,3306,5432,6379,9200,11211,27017)
+        -m 指定线程数量 默认100线程
+        -t 指定请求超时时间。
+        -d 指定密码字典。
+        -n 不进行存活探测(ICMP)直接进行扫描。
+Example:
+        python Scrack.py -h 10.111.1
+        python Scrack.py -h 192.168.1.1 -d pass.txt
+        python Scrack.py -h 10.111.1.1-10.111.2.254 -p 3306,5432 -m 200 -t 6
+        python NAScan.py -h ip.ini -n
+
     '''
     if len(sys.argv) < 2:
         print msg
